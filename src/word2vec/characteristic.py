f8a7b6c13596180bb4eae85aae4d742fb1c18dcc
lecheng <lechengbupt@gmail.com>
2017-04-08 22:50:37 -0700

    init characteristic model

---
+++ src/word2vec/characteristic.py
@@ -0,0 +1,21 @@
+class Characteristic:
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def get_similarity(self,w1,w2):
+        '''
+        Calculate the similarity of two words
+        :param w1: (String) the first word to calculate similarity
+        :param w2: (String) the second word to calculate similarity
+        :return: (Double) similarity of the two words
+        '''
+        pass
+
+    def recognize(self,morph):
+        '''
+        Recognize morph
+        :param morph:
+        :return: (list) a list of possible objects of the morph
+        '''
+        pass
