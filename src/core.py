8dc1621fbde8899962f17591e75cfa8295791bb6
pwwpcheng <pwwp@outlook.com>
2017-04-09 20:04:44 -0700

    PhoneticSubstitution fully implemented. Dictionary files for SpellingDecomposition added.

--- src/core.py
+++ src/core.py
@@ -12,11 +12,11 @@ class EMRecognition:
         '''
 
         '''Recognition_modules stores classes of every method for EMR recognition'''
-        recognition_modules = [PhoneticSubstitution]
+        self.recognition_modules = [PhoneticSubstitution]
 
         ''' recognition_objects stores instances of every module as
-            {<module_name>: <module_object>}'''
-        recognition_objects = {}
+            {<module_name>: ['object': <module_object>, 'confidence': <float>], ...}'''
+        self.recognition_objects = {}
 
         ''' logger prints and stores log files and is passed on to every instance'''
         logger = Logger()
@@ -27,11 +27,27 @@ class EMRecognition:
             'logger': logger
         }
 
-        for module in recognition_modules:
+        for module in self.recognition_modules:
             module_name = module.__name__
-            recognition_objects[module_name] = module(*args,  **kwargs)
+            self.recognition_objects[module_name] = {
+                'object': module(*args,  **kwargs),
+                'confidence': 1.0
+            }
 
         pass
+
+    def recognize_tweet(self, tweet):
+        '''
+        Extract name entity morphs from a tweet.
+        :param tweet: (String) content of the tweet(weibo)
+        :return: (Two dimensional array) [[<String>, <float>], ...]
+                Explanation: [[<morph>, <confidence>], [<morph>, <confidence>], ...]
+                Every inner array has two components, a string and a float number.
+                The string represents possible name morphs;
+                The float number represents confidence calculate by our model.
+        '''
+        pass
+
 
 
 if __name__ == '__main__':
--- src/spelling_decomposition.py
+++ src/spelling_decomposition.py
@@ -3,20 +3,71 @@ import os
 
 from settings import config
 from src.logger import Logger, EmptyLogger
+from src.common import CN_CHAR_REGEX
 
 class SpellingDecomposition:
     def __init__(self, *args, **kwargs):
 
         self.logger = kwargs['logger']
-        self.division_dict = None
+        self.use_raw_dict = kwargs.get('use_raw_dict', True)
 
+        self.decomposition_dict, self.inverse_decomposition_dict = {}, {}
+        # Read decomposition dict
         try:
-            with open(os.path.join(config.DICT_ROOT, "word_division_dict"), encoding='utf-8') as f:
-                self.division_dict = json.load(f)
+            if self.use_raw_dict:
+                file_handler = open(os.path.join(config.SPELLING_ROOT, "division_raw.txt"), encoding='utf-8')
+                self._load_raw_dict(file_handler)
+                file_handler.close()
+            else:
+                with open(os.path.join(config.DICT_ROOT, "word_division_dict"), encoding='utf-8') as f:
+                    self.decomposition_dict = json.load(f)
         except FileExistsError as e:
             self.logger.error("word_division_dict does not exist. Division dictionary not loaded.")
         except json.decoder.JSONDecodeError as e:
             self.logger.error("Error while parsing division dictionary from word_division_dict.")
+
+        self.stem_similar_chars = {}
+        # Read stem_similar_characters_dict
+        try:
+            file_handler = open(os.path.join(config.SPELLING_ROOT, "division_raw.txt"), encoding='utf-8')
+            self._load_raw_dict(file_handler)
+            file_handler.close()
+        except FileExistsError as e:
+            self.logger.warning("stem_similar_characters_dict does not exist.")
+        except SyntaxError as e:
+            self.logger.error("Error while parsing division dictionary from word_division_dict.")
+
+
+    def _load_stem_character_dict(self, file_handler):
+        '''
+        Load character division from division_raw.txt
+        :param file_handler: (Python file handler)
+        :return: None
+        '''
+        for line in file_handler:
+            cn_character = CN_CHAR_REGEX.findall(line)
+            character = cn_character[0]
+            stems = cn_character[1:]
+            self.decomposition_dict[character] = stems
+
+
+    def _load_raw_dict(self, file_handler):
+        '''
+        Load similar character for stems from division_raw.txt
+        :param file_handler: (Python file handler)
+        :return: None
+        '''
+        for line in file_handler:
+            cn_character = CN_CHAR_REGEX.findall(line)
+            if len(cn_character) > 1:
+                ori_char = cn_character[0]
+                decomp_chars = "".join(cn_character[1:])
+                self.decomposition_dict[ori_char] = decomp_chars
+                if decomp_chars not in self.inverse_decomposition_dict:
+                    self.inverse_decomposition_dict[decomp_chars] = [ori_char]
+                else:
+                    self.inverse_decomposition_dict[decomp_chars].append(ori_char)
+
 
     def get_similarity_score(self, str_a, str_b):
         '''
@@ -28,7 +79,20 @@ class SpellingDecomposition:
                          are exactly the same, the score would be either 0 or 1.
         '''
 
+        if str_a == str_b:
+            return 1.0
+
+        # Since spelling decomposition is specifically designed for Chinese characters
+        # All non_chinese characters will be filtered out before processing
+        str_a = "".join(CN_CHAR_REGEX.findall(str_a))
+        str_b = "".join(CN_CHAR_REGEX.findall(str_b))
+
+
 
     def get_similar_names(self, name):
         pass
 
+
+if __name__ == "__main__":
+    empty_logger = EmptyLogger()
+    spd = SpellingDecomposition(logger=empty_logger, use_raw_dict=True)
