25012ca73989670324a61a4924a586272d2b0824
adaZ9422 <yihezhan@usc.edu>
2017-04-11 13:06:15 -0700

    updata translation

--- src/translation_and_transliteration.py
+++ src/translation_and_transliteration.py
@@ -1,10 +1,15 @@
+ #-*- coding: UTF-8 -*-   
 from translate import Translator
-import jieba
+import heapq
 
 class Translation:
-    def __init__(self):
+    def __init__(self,namelist):
+        #self.logger = kwargs['logger']
         self.ec_translator = Translator(to_lang="zh")
         self.ce_translator = Translator(from_lang="zh",to_lang="en")
+
+        self.name_list = namelist
+        self.treshold = 10
     
     def get_levenshtein_distance(self,str_a,len_a,str_b,len_b):
         '''
@@ -56,18 +61,46 @@ class Translation:
         max=0
         if(self.calculate_similarity_score(str_a,str_b)>max):
             max = self.calculate_similarity_score(str_a,str_b)
-        seg_list = jieba.cut(str_b)
+        eng_part = self.ce_translator.translate(str_b)
+        ch_part = self.ce_translator.translate(str_a)
+        seg_list = eng_part.split(" ")
         for i in seg_list:
-            if(self.calculate_similarity_score(str_a,i)>max):
-                max = self.calculate_similarity_score(str_a,i)
+            if(self.calculate_similarity_score(ch_part,i)>max):
+                max = self.calculate_similarity_score(ch_part,i)
         return max
 
+    def get_similar_names(self, name):
+        '''
+        Predict top 10 of possible name from celevrity list for an entity morph
+        :param name: the given morph
+        :return:(Dict){<name>: <confidence score>, ...}
+                       <name> : (String) name ofpossible entity morph
+                       <confidence_score>: (float)
+        '''
+        poss_names = []
+        for name_entity in self.name_list:
+            score = self.get_similarity_score(name,name_entity)
+            if len(poss_names) < self.treshold:
+                heapq.heappush(poss_names,(score,name_entity))
+            else:
+                if score > poss_names[0][0]:
+                    heapq.heappop(poss_names)
+                    heapq.heappush(poss_names,(score,name_entity))
+
+        poss_names = sorted(poss_names, key=lambda x: x[0], reverse=True)
+
+        return {name: score for (score, name) in poss_names}
+
+
+
+            
+
 def main(defvals=None):
-    l = Translation()
-    #print(l.get_similarity_score("拉里大鸟","拉里伯德"))
-    #print(l.calculate_similarity_score("大鸟","伯德"))
-    print(l.ce_translator.translate("大鸟"))
-    print(l.ce_translator.translate("伯德"))
+    names = ["泰勒斯威夫特", "蕾哈娜", "酷玩", "贾斯汀比伯", "恩雅", "老鹰", "林肯公园","拉里伯德"]
+    l = Translation(names)
+    print(l.get_similar_names("鸟儿"))
+    #print(l.get_similarity_score("鸟儿","拉里伯德"))
+    #print(l.ce_translator.translate("伯德"))
         
 if __name__=="__main__":
     main()
