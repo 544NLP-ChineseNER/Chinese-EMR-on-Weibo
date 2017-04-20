52143883f345a70e4b9de95f3f6651eb5365c793
lecheng <lechengbupt@gmail.com>
2017-04-19 21:42:32 -0700

    add NER and online learning of word2vec training

--- src/characteristic.py
+++ src/characteristic.py
@@ -2,6 +2,7 @@ from gensim.models import Word2Vec
 import logging
 import os
 from settings.config import WORD2VEC_ROOT
+import jieba.posseg as pseg
 
 class Characteristic:
 
@@ -16,7 +17,7 @@ class Characteristic:
         :return:
         '''
         try:
-            self.model = Word2Vec.load(os.path.join(WORD2VEC_ROOT,'wiki.word2vec.model'))
+            self.model = Word2Vec.load(os.path.join(WORD2VEC_ROOT,'news.word2vec.model'))
         except FileNotFoundError as e:
             self.logger.error(e)
             raise
@@ -36,13 +37,14 @@ class Characteristic:
         except KeyError as e:
             self.logger.error(e)
 
-    def get_related(self,word):
+    def get_related(self,word,N=20):
         '''
         :param word:
         :return: (list) a list of related words
         '''
+        names = []
         try:
-            names = self.model.similar_by_word(word, topn=20)
+            names = self.model.similar_by_word(word, topn=N)
         except KeyError as e:
             self.logger.error(e)
         for name in names:
@@ -55,13 +57,22 @@ class Characteristic:
         :param morph:
         :return: (list) a list of possible objects of the morph
         '''
-        pass
+        names = []
+        res = []
+        try:
+            names = self.model.similar_by_word(word, topn=10)
+        except KeyError as e:
+            self.logger.error(e)
+        for name in names:
+            print(name[0], ' ', name[1])
+            res += [name[0]]
+        return res
 
 
 if __name__ == '__main__':
     c = Characteristic()
     c.load_model()
-    query = "姚明"
-    query2 = "杨幂"
-    c.get_related(query)
+    query = "小巨人" #O'Neal
+    query2 = "姚明" #Kobe
+    c.get_related(query,20)
     c.get_similarity_score(query,query2)
--- src/word2vec/w2vtrain.py
+++ src/word2vec/w2vtrain.py
@@ -3,6 +3,7 @@ import jieba
 import multiprocessing
 import os
 import time
+import shutil
 
 from logging import Logger
 from gensim.corpora import WikiCorpus
@@ -26,9 +27,25 @@ class ChineseWikiTrain:
         print(os.path.join(config.WORD2VEC_ROOT ,"zhwiki-latest-pages-articles.xml.bz2"))
         self.output_path = kwargs.get("output", config.WORD2VEC_ROOT)
 
-    def xml_to_txt(self):
+    def news_xml_to_txt(self,path):
         '''
-        transfer xml version data into txt version
+        transfer news data into text version
+        '''
+        path = os.path.join(self.output_path,'news_tensite_xml.dat')
+        with open(path,'rb') as f1:
+            with open('src/word2vec/news.dat','wb') as f2:
+                lines = f1.readlines()
+                for line in lines:
+                    words = " ".join(jieba.cut(line))
+                    if 'content' in  words and 'contentitle' not in words:
+                        words = words.replace('contenttitle','').replace('content','').replace('<','').replace('>','').replace('/','')
+                        f2.write(words.encode('utf-8'))
+                f2.close()
+            f1.close()
+        
+    def wiki_xml_to_txt(self):
+        '''
+        transfer wiki xml version data into txt version
         '''
         self.logger.info("transfering xml to txt...")
         inp = self.input_path
@@ -64,14 +81,15 @@ class ChineseWikiTrain:
         f.close()
         self.logger.info('convertion finished!')
 
-    def tokenize(self):
+    def tokenize(self,inp):
         '''
         tokenize sentence into words
         '''
         self.logger.info('tokenization start...')
         jieba.enable_parallel(8)
-        inp = os.path.join(self.output_path, 'wiki.zh.text.simplified')
-        output = os.path.join(self.output_path, 'wiki.zh.text.simplified_seg')
+        # inp = os.path.join(self.output_path, 'wiki.zh.text.simplified')
+        # output = os.path.join(self.output_path, 'wiki.zh.text.simplified_seg')
+        output = inp + "_seg"
 
         log_f = open(output, "wb")
 
@@ -86,13 +104,14 @@ class ChineseWikiTrain:
         log_f.close()
 
 
-    def train(self):
+    def train(self,path):
         '''
         train word2vec from wiki
         '''
         self.logger.info('start training...')
-        inp = os.path.join(self.output_path, 'wiki.zh.text.simplified')
-        outp1 = os.path.join(self.output_path, 'wik.word2vec.model')
+        # inp = os.path.join(self.output_path, 'wiki.zh.text.simplified')
+        inp = path
+        outp1 = os.path.join(self.output_path, 'news.word2vec.model')
 
         model = Word2Vec(LineSentence(inp), size=300, window=5, min_count=5,
                          workers=multiprocessing.cpu_count(), iter=3)
@@ -100,9 +119,30 @@ class ChineseWikiTrain:
         model.save(outp1)
         self.logger.info('training finished!')
 
+    def continue_train(self,inp):
+        '''
+        train new model based on loaded model
+        '''
+        modelpath = os.path.join(self.output_path,'news.word2vec.model')
+
+        # backup old model
+        shutil.copy(modelpath,modelpath+'.old')
+        shutil.copy(modelpath,modelpath+'.old.syn1neg.npy')
+        shutil.copy(modelpath,modelpath+'.old.wv.syn0.npy')
+
+        model = Word2Vec.load(modelpath)
+        print(len(model.wv.vocab))
+        print(LineSentence(inp))
+        model.build_vocab(LineSentence(inp),update=True)
+        print(len(model.wv.vocab))
+        model.train(LineSentence(inp),total_examples=model.corpus_count)
+        model.save(modelpath)
+        self.logger.info('continuing training finished!')
+
 if __name__ == '__main__':
     t = ChineseWikiTrain()
-    t.xml_to_txt()
-    t.trad_to_simp()
-    t.tokenize()
-    t.train()
+    # t.wiki_xml_to_txt()
+    # t.trad_to_simp()
+    # t.tokenize(inp = os.path.join(t.output_path,'wiki.zh.text.simplified'))
+    t.continue_train(os.path.join(t.output_path,'morph.dat'))
+    # t.train(os.path.join(t.output_path, 'news.dat'))
