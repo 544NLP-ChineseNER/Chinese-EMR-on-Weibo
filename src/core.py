d38d2c5efa31a727a1ad8cb9a0fd72fec586efeb
lecheng <lechengbupt@gmail.com>
2017-04-28 16:11:50 -0700

    add patterns

--- src/core.py
+++ src/core.py
@@ -23,7 +23,6 @@ class EMRecognition:
 
         '''Recognition_modules stores classes of every method for EMR recognition'''
         self.recognition_classes = [PhoneticSubstitution, NicknameGeneration, SpellingDecomposition, Translation, Characteristic]
-
         ''' recognition_objects stores instances of every module as
             {<module_name>: ['object': <module_object>, 'confidence': <float>], ...}'''
         self.recognition_modules = {}
