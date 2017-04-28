c487ca2f0a408e8d8c576d82d6ffbf80f065c3dd
adaZ9422 <yihezhan@usc.edu>
2017-04-28 01:05:33 -0700

    update get_similar_names

--- src/translation_and_transliteration.py
+++ src/translation_and_transliteration.py
@@ -2,13 +2,13 @@
 import heapq
 from urllib.request import urlopen,quote
 import json
+import enchant
 
 from src.logger import Logger, EmptyLogger
 
 class Translation:
     def __init__(self, *args, **kwargs):
 
-        #self.ce_translator = Translator(from_lang="zh",to_lang="en")
 
         try:
             self.name_list = kwargs.get("name_list")
@@ -21,6 +21,8 @@ class Translation:
 
         self.url = ""
         self.treshold = 10
+        self.poss_names = []
+        self.dict = enchant.Dict("en_US")
 
     def set_url(self,name):
         self.url = "http://fanyi.youdao.com/openapi.do?keyfrom=csci544project&key=1099532634&type=data&doctype=json&version=1.1&q="+quote(name)
@@ -91,35 +93,56 @@ class Translation:
                        <name> : (String) name ofpossible entity morph
                        <confidence_score>: (float)
         '''
-        poss_names = []
-        for name_entity in self.name_list:
-            score = self.get_similarity_score(name,name_entity)
-            if len(poss_names) < self.treshold:
-                heapq.heappush(poss_names,(score,name_entity))
-            else:
-                if score > poss_names[0][0]:
-                    heapq.heappop(poss_names)
-                    heapq.heappush(poss_names,(score,name_entity))
+        eng_name = json.loads(urlopen(self.set_url(name)).read().decode("utf-8").encode("utf-8"))['translation'][0];
+        splitted_word = eng_name.split(" ")
+        #splitted_word = ["tiger"]
+        position = len(splitted_word)
+        for word in splitted_word:
+            self.get_word_score(word,position,0)
+            ed1 = self.edit1(word)
+            for ed in ed1:
+                if self.dict.check(ed):
+                    self.get_word_score(ed,position,1)
+            position-=1
+        self.poss_names = sorted(self.poss_names, key=lambda x: x[0], reverse=True)
 
-        poss_names = sorted(poss_names, key=lambda x: x[0], reverse=True)
+        return {name: score for (score, name) in self.poss_names[:5]}
 
-        return {name: score for (score, name) in poss_names}
+    def get_word_score(self,word,position,ed):
+        sim = 1-ed/len(word)
+        translate = json.loads(urlopen(self.set_url(word)).read().decode("utf-8").encode("utf-8"))
+        if 'basic' in translate:
+            explains = translate['basic']['explains']
+            print(explains)
+            for ex in explains:
+                exs = ex.split("；")
+                if '人名' in exs[0]:
+                    for x in range(1, len(exs)):
+                        heapq.heappush(self.poss_names, (1/position*sim,exs[x][exs[x].find(")") + 1:]))
+        return
 
-
-
-            
+    def edit1(self,word):
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        splits = [(word[:i],word[i:]) for i in range(len(word)+1)]
+        deletes = [L + R[1:] for L, R in splits if R]
+        transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+        replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+        inserts = [L + c + R for L, R in splits for c in letters]
+        return set(deletes+transposes+replaces+inserts)
 
 def main(defvals=None):
-    names = ["泰勒斯威夫特", "蕾哈娜", "酷玩", "贾斯汀比伯", "恩雅", "老鹰", "林肯公园","拉里伯德"]
-    l = Translation(names)
+    names = ["泰勒斯威夫特", "蕾哈娜", "酷玩", "贾斯汀比伯", "恩雅", "老鹰", "林肯公园","拉里伯德","泰格伍兹"]
+    l = Translation(name_list=names)
     #name = quote('大鸟')
     #print(name)
     #result = urlopen("http://fanyi.youdao.com/openapi.do?keyfrom=csci544project&key=1099532634&type=data&doctype=json&version=1.1&q="+name).read().decode("utf-8").encode("utf-8")
     #print(json.loads(result)['translation'][0])
-    print(l.get_similar_names("大鸟"))
+    print(l.get_similar_names("鸟"))
     #print(l.get_similarity_score("鸟","拉里伯德"))
     #print(l.calculate_similarity_score("larry bird","the bird"))
-    #print(json.loads(urlopen(l.set_url("拉里伯德")).read().decode("utf-8").encode("utf-8"))['translation'][0])
+    #print(json.loads(urlopen(l.set_url("老虎")).read().decode("utf-8").encode("utf-8"))['translation'][0])
+    #x = json.loads(urlopen(l.set_url("tiger")).read().decode("utf-8").encode("utf-8"))['basic']['explains'][1].split("；")[1]
+    #print(x[x.find(")")+1:])
     #print(l.ce_translator.translate("伯德"))
     #print(Translator(from_lang="zh",to_lang="en").translate("伯德"))
         
